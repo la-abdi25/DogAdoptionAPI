42cbc3831e9c896e1cc9c4de618b91880f9b8323
la-abdi25 <labdi0335@gmail.com>
2025-08-19 16:00:57 -0500

    updated app.js

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use(userRoutes);
 app.use(dogRoutes);
 app.use(messageRoutes);
 
+//main landing page
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Welcome to the Dog Adoption API" });
+});
 //start the server to start listening on PORT 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
